e572bdbcfeb15777f18d13340b8b08572f182f29
Poof <130206494+goodmorningpoof@users.noreply.github.com>
2025-05-24 14:34:51 -0500

    Update 139-Word-Break-Medium.py

--- 139-Word-Break-Medium.py
+++ 139-Word-Break-Medium.py
@@ -1,5 +1,8 @@
 # https://leetcode.com/problems/word-break/
 
+# i tried to comment my code, but if youre still lost, theres a video i made for this a bit ago:
+# https://youtu.be/owCi6ZUWCTA?si=xKrdFtS77BRe18Z7
+
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
 
