c25fdee6edc16ef950e63ac9f47d8723b41805f6
Poof <130206494+goodmorningpoof@users.noreply.github.com>
2025-07-17 16:49:08 -0500

    Update 199-Binary-Tree-Right-Side-View-Medium.py

--- 199-Binary-Tree-Right-Side-View-Medium.py
+++ 199-Binary-Tree-Right-Side-View-Medium.py
@@ -1,6 +1,7 @@
 # link: https://leetcode.com/problems/binary-tree-right-side-view/
 
 # Go level by level, take right-most node and add to res array, pretty simple me thinks
+# idk why you would need a video for this problem, but if you do: https://www.youtube.com/watch?v=7FXEPyvIN3Y
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
 
