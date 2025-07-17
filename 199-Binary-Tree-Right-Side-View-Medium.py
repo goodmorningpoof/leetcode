90a81e7b4fe007c87d5866d6f4886ec869691d08
Poof <130206494+goodmorningpoof@users.noreply.github.com>
2025-07-17 16:07:22 -0500

    Create 199-Binary-Tree-Right-Side-View-Medium.py

---
+++ 199-Binary-Tree-Right-Side-View-Medium.py
@@ -0,0 +1,30 @@
+# link: https://leetcode.com/problems/binary-tree-right-side-view/
+
+# Go level by level, take right-most node and add to res array, pretty simple me thinks
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+
+        # base case check
+        if not root:
+            return []
+
+        queue = collections.deque([root])
+        res = []
+        while queue:
+            level_len = len(queue)
+
+            for i in range(level_len):
+                node = queue.popleft()
+                
+                if i == level_len - 1: #right mode node
+                    res.append(node.val)
+                
+                if node.left:
+                    queue.append(node.left)
+                
+                if node.right:
+                    queue.append(node.right)
+                
+        return res
+        
+        
